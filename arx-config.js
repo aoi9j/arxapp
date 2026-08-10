a3cb0fcd1d6a7cf89e6e4a204895b1c9f5cfdae2
aoi9j <jenialjarilla9@icloud.com>
2026-08-10 16:59:15 +0200

    Fix typo in ARX_SUPABASE_URL

--- arx-config.js
+++ arx-config.js
@@ -1,2 +1,2 @@
-window.ARX_SUPABASE_URL = "https://hxpflyblflmimfeuwcvz.supabase.co";
+window.ARX_SUPABASE_URL = "https://hxpflybflfmimfeuwcvz.supabase.co";
 window.ARX_SUPABASE_KEY = "sb_publishable_OQjVeuUA0ukwLs-klalLAg_jOXa2E7t";
